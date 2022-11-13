3c495f1e32ab58d4cf5f67ed417b37c14d81a1e1
Tairara <114944188+Tairara@users.noreply.github.com>
2022-11-13 08:08:17 +0300

    Update Question4.java

--- task_one/Question4.java
+++ task_one/Question4.java
@@ -27,10 +27,11 @@ public class Question4 {
             if (myList.get(i) % 2 == 0) {
                 System.out.println(myList.get(i));
 
-                if (i == 150 - 71) {
+            }
+            
+            if (i == 150 - 71) {
                     break;
                 }
-            }
 
         }
     }
@@ -41,11 +42,10 @@ public class Question4 {
         for (int i = 0; i <= myList.size(); i++) {
             if (myList.get(i) % 3 == 0) {
                 System.out.println(myList.get(i));
-
-                if (i == 150 - 71) {
+            }
+            if (i == 150 - 71) {
                     break;
                 }
-            }
 
         }
     }
@@ -57,10 +57,10 @@ public class Question4 {
             if (myList.get(i) % 7 == 0) {
                 System.out.println(myList.get(i));
 
-                if (i == 150 - 71) {
+            }
+            if (i == 150 - 71) {
                     break;
                 }
-            }
 
         }
     }
